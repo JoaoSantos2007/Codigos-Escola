c34ea4d20630dda981f38002d69f9b9152ff7d7b
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-03-17 13:09:37 -0300

    mais arquivos

---
+++ Codigos/ProblemadasTomadas.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t1,t2,t3,t4,total;
+    cin>>t1>>t2>>t3>>t4;
+    total = t1+t2+t3+t4-3;
+    cout<<"O total de tomadas é: "<<total;
+}
