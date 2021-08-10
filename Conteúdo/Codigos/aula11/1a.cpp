02997bd1ab6861c31fe85f58699d889010783dde
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-08-10 14:01:42 -0300

    update

---
+++ Conteúdo/Codigos/aula11/1a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m = 4;
+    if(m != 5){
+        cout<<"verdadeiro!"<<endl;
+    }else{
+        cout<<"falso!"<<endl;
+    }
+}
---
+++ Conteúdo/Codigos/aula11/1c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m = 10;
+    if(m < 10){
+        cout<<"verdadeiro!"<<endl;
+    }else{
+        cout<<"falso!"<<endl;
+    }
+}
